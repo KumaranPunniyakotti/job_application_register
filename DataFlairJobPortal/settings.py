c4e38417bfcfa053f5c13d7aa55a002b6a588760
KumaranPunniyakotti <kumaranpunniyakotti@gmail.com>
2021-04-28 20:13:01 +0530

    staic setting upgradations

--- DataFlairJobPortal/settings.py
+++ DataFlairJobPortal/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 MIDDLEWARE_CLASSES = (
     # Simplified static file serving.
